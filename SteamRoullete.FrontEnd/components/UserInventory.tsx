ee898bc90d1cf8c1280eef3565bf0cb6be44858d
GodObject <vladimirgjob@gmail.com>
2024-12-05 11:06:35 +0300

    Rename SpeedSlider.tsx to inventory

---
+++ SteamRoullete.FrontEnd/components/UserInventory.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface UserInventoryProps {}
+
+const UserInventory: FC<UserInventoryProps> = () => {
+  return (
+    <>
+      <span>Inventory</span>
+    </>
+  );
+};
+
+export default UserInventory;
